d7071f71e9b258a610ad22f7fba53cd09d3f8d90
DmitriyIvanko <dmitriy.ivanko@protonmail.com>
2025-05-10 01:27:26 +0200

    PreinstalledSoftwhareAndLicenceModel

--- Program.cs
+++ Program.cs
@@ -1,21 +1,15 @@
-
-
 using OpenTelemetry;
 using OpenTelemetry.Exporter;
 using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
-
 using Amazon;
 using Amazon.EC2;
 using Amazon.EC2.Model;
 using Amazon.Runtime;
-using Microsoft.Extensions.Configuration;
+//using Amazon.LicenseManager;
+//using Amazon.LicenseManager.Model;
 using Amazon.Util;
-using System;
-using System.Threading.Tasks;
-
 using System.Reflection;
-using Microsoft.VisualBasic;
 using Microsoft.Extensions.Caching.Memory;
 
 namespace Ec2TraceGenerator;
@@ -123,10 +117,62 @@ public static class Program {
             RegionEndpoint = region
         });
 
+        // Initialize License Manager client
+        //var licenseManagerClient = new AmazonLicenseManagerClient(credentials, new AmazonLicenseManagerConfig
+        //{
+        //    RegionEndpoint = region
+        //});
+
         // Describe instance request
+        if (!memoryCache.TryGetValue(EC2InstanceMetadata.InstanceId, out Instance? instance) || instance is null)
+        {
+            instance = await DescripeInstancesApiCall(client, EC2InstanceMetadata.InstanceId);
+            memoryCache.Set(EC2InstanceMetadata.InstanceId, instance, new MemoryCacheEntryOptions());
+        }
+
+        // Clarify platform details request
+        if (!memoryCache.TryGetValue(EC2InstanceMetadata.InstanceId, out string? platformDetails) || platformDetails is null)
+        {
+            platformDetails = await ClarifyPlatformDetailsAsync(client, instance.ImageId, memoryCache, instance.PlatformDetails);
+            memoryCache.Set(EC2InstanceMetadata.InstanceId, platformDetails, new MemoryCacheEntryOptions());
+        }
+
+        // Describe capacity reservation request to ger capacity reservation preference
+        if (!memoryCache.TryGetValue(EC2InstanceMetadata.InstanceId, out string? capacityReservationPreference)
+            || string.IsNullOrEmpty(capacityReservationPreference))
+        {
+            capacityReservationPreference = await DescribeCapacityReservationsApiCall(client, instance);
+            memoryCache.Set(EC2InstanceMetadata.InstanceId, capacityReservationPreference, new MemoryCacheEntryOptions());
+        }
+
+        // Describe fleets request to get the fleet id
+        if (!memoryCache.TryGetValue(EC2InstanceMetadata.InstanceId, out string? fleetId)
+            || string.IsNullOrEmpty(fleetId))
+        {
+            fleetId = await GetFleetIdApiCall(client, instance);
+            memoryCache.Set(EC2InstanceMetadata.InstanceId, fleetId, new MemoryCacheEntryOptions());
+        }
+
+        return new Dictionary<string, object>
+        {
+            ["aws.ec2.instance_id"] = instance.InstanceId,
+            ["aws.ec2.instance_type"] = instance.InstanceType.Value,
+            ["aws.region"] = regionName,
+            ["aws.ec2.platform_details"] = platformDetails,
+            ["aws.ec2.license_model"] = instance.Licenses.Count == 0 ? "No License required" : "Bring your own license", // Though I'm very unsure of it
+            ["aws.ec2.tenancy"] = instance.Placement.Tenancy.Value,
+            ["aws.ec2.instance_lifecycle"] = instance.InstanceLifecycle.Value,
+            ["aws.ec2.capacity_reservation_id"] = instance.CapacityReservationId,
+            ["aws.ec2.capacity_reservation_preference"] = capacityReservationPreference,
+            ["aws.ec2.fleet_id"] = fleetId
+        };
+    }
+
+    static async Task<Instance> DescripeInstancesApiCall(AmazonEC2Client client, string instanceId)
+    {
         var request = string.IsNullOrWhiteSpace(EC2InstanceMetadata.InstanceId)
         ? new DescribeInstancesRequest()
-        : new DescribeInstancesRequest { InstanceIds = new List<string> { EC2InstanceMetadata.InstanceId } };
+        : new DescribeInstancesRequest { InstanceIds = new List<string> { instanceId } };
 
         var response = await client.DescribeInstancesAsync(request);
 
@@ -139,9 +185,11 @@ public static class Program {
             throw new InvalidOperationException(message);
         }
 
-        var instance = response.Reservations.First().Instances.First();
+        return response.Reservations.First().Instances.First();
+    }
 
-        // Describe capacity reservation request to ger capacity reservation preference
+    static async Task<string> DescribeCapacityReservationsApiCall(AmazonEC2Client client, Instance instance)
+    {
         var capacityReservationPreference = "none";
 
         if (!string.IsNullOrEmpty(instance.CapacityReservationId))
@@ -160,48 +208,76 @@ public static class Program {
             }
         }
 
-        // Describe fleets request to get the fleet id
+        return capacityReservationPreference;
+    }
+
+    static async Task<string> GetFleetIdApiCall(AmazonEC2Client client, Instance instance)
+    {
         var fleetId = string.Empty;
 
         var describeFleetsRequest = new DescribeFleetsRequest();
         var fleetResponse = await client.DescribeFleetsAsync(describeFleetsRequest);
 
-        if (memoryCache.TryGetValue(instance.InstanceId, out string? fleetIdFromCache) && !string.IsNullOrEmpty(fleetIdFromCache))
+        if (fleetResponse.Fleets != null && fleetResponse.Fleets.Count == 0)
         {
-            fleetId = fleetIdFromCache;
-        }
-        else
-        {
-            if (fleetResponse.Fleets != null && fleetResponse.Fleets.Count == 0)
+            foreach (var fleet in fleetResponse.Fleets)
             {
-                foreach (var fleet in fleetResponse.Fleets)
+                var fleetInstancesRequest = new DescribeFleetInstancesRequest
                 {
-                    var fleetInstancesRequest = new DescribeFleetInstancesRequest
-                    {
-                        FleetId = fleet.FleetId
-                    };
-                    var fleetInstancesResponse = await client.DescribeFleetInstancesAsync(fleetInstancesRequest);
-                    if (fleetInstancesResponse.ActiveInstances.Any(i => i.InstanceId == instance.InstanceId))
-                    {
-                        fleetId = fleet.FleetId;
-                        memoryCache.Set(instance.InstanceId, fleetId, new MemoryCacheEntryOptions());
-                        break;
-                    }
+                    FleetId = fleet.FleetId
+                };
+                var fleetInstancesResponse = await client.DescribeFleetInstancesAsync(fleetInstancesRequest);
+                if (fleetInstancesResponse.ActiveInstances.Any(i => i.InstanceId == instance.InstanceId))
+                {
+                    fleetId = fleet.FleetId;
+                    break;
                 }
             }
         }
 
-        return new Dictionary<string, object>
+        return fleetId;
+    }
+
+    static async Task<string> ClarifyPlatformDetailsAsync(AmazonEC2Client client, string imageId, IMemoryCache memoryCache, string originalPlatformDetails)
+    {
+        string[] platformDetailsWithoutOs = 
+        [
+            "SQL Server Standard",
+            "SQL Server Enterprise",
+            "SQL Server Web"
+        ];
+
+        if (platformDetailsWithoutOs.Contains(originalPlatformDetails))
         {
-            ["aws.ec2.instance_id"] = instance.InstanceId,
-            ["aws.ec2.instance_type"] = instance.InstanceType.Value,
-            ["aws.region"] = regionName,
-            ["aws.ec2.operating_system"] = instance.PlatformDetails,
-            ["aws.ec2.tenancy"] = instance.Placement.Tenancy.Value,
-            ["aws.ec2.instance_lifecycle"] = instance.InstanceLifecycle.Value,
-            ["aws.ec2.capacity_reservation_id"] = instance.CapacityReservationId,
-            ["aws.ec2.capacity_reservation_preference"] = capacityReservationPreference,
-            ["aws.ec2.fleet_id"] = fleetId
-        };
+            if (!string.IsNullOrEmpty(imageId))
+            {
+                var describeImagesRequest = new DescribeImagesRequest
+                {
+                    ImageIds = new List<string> { imageId }
+                };
+                var describeImagesResponse = await client.DescribeImagesAsync(describeImagesRequest);
+                if (describeImagesResponse.Images.Count > 0)
+                {
+                    var image = describeImagesResponse.Images[0];
+                    var name = image.Name?.ToLower() ?? "";
+                    var description = image.Description?.ToLower() ?? "";
+                    var imagePlatformDetails = image.PlatformDetails ?? originalPlatformDetails;
+
+                    if (originalPlatformDetails.StartsWith("SQL Server"))
+                    {
+                        if (name.Contains("amzn") || description.Contains("amazon linux"))
+                            return "Amazon Linux 2 with " + originalPlatformDetails;
+                        if (name.Contains("windows") || description.Contains("windows"))
+                            return "Windows with " + originalPlatformDetails;
+                        if (name.Contains("ubuntu") || description.Contains("ubuntu"))
+                            return "Ubuntu with " + originalPlatformDetails;
+                        if (name.Contains("rhel") || description.Contains("rhel") || name.Contains("red hat") || description.Contains("red hat"))
+                            return "Red Hat Enterprise Linux with " + originalPlatformDetails;
+                    }
+                    return imagePlatformDetails;
+                }
+            }
+        }
+        return originalPlatformDetails; // Fallback if AMI lookup fails
     }
 }
